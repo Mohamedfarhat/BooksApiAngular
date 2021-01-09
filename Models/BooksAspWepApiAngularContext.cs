11497651231b2d9ee14acdf7e044e9b6594c5115
Mohamed Farhat <mohamedfarhat01@gmail.com>
2021-01-09 17:50:08 +0400

    Add project files.

---
+++ Models/BooksAspWepApiAngularContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace BooksApiAngular.Models
+{
+    public partial class BooksAspWepApiAngularContext : DbContext
+    {
+        public BooksAspWepApiAngularContext()
+        {
+        }
+
+        public BooksAspWepApiAngularContext(DbContextOptions<BooksAspWepApiAngularContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Book> Book { get; set; }
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,66 @@
+using BooksApiAngular.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksApiAngular
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "BooksApiAngular", Version = "v1" });
+            });
+            services.AddDbContext<BooksAspWepApiAngularContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("BookDb")));
+            services.AddCors(options => options.AddPolicy("CorsPolicy", builder =>
+               {
+                   builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+               }
+            ));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BooksApiAngular v1"));
+            }
+
+            app.UseRouting();
+            app.UseCors("CorsPolicy");
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
